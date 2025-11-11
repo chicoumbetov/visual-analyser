6f1ca3abfdc8888403d1d2fca693908111014185
chicoumbetov <shyngysmaaan@gmail.com>
2025-11-11 20:52:17 +0100

    :lipstick: fix auth page style

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,9 +3,13 @@ import type { Metadata } from 'next'
 import { Auth } from './Auth'
 
 export const metadata: Metadata = {
-	title: 'Authorization'
+  title: 'Authorization'
 }
 
 export default function AuthPage() {
-	return <Auth />
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full py-8">
+      <Auth />
+    </div>
+  )
 }
