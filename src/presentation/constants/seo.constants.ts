5835a37951fc3512c4422e24dc10cfc2553820f0
chicoumbetov <shyngysmaaan@gmail.com>
2025-11-11 17:20:56 +0100

    :sparkles: layout

---
+++ src/presentation/constants/seo.constants.ts
@@ -0,0 +1,5 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
+
+export const SITE_NAME = 'analysis'
+export const SITE_DESCRIPTION =
+	'Welcome to our app - a unique platform for electrical pipeline analysis, designed with modern trends and technologies in mind.'
