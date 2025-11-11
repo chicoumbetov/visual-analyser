cc6afa8e37bae5524e22e5cc11be52b0619c6734
chicoumbetov <shyngysmaaan@gmail.com>
2025-11-11 21:47:20 +0100

    fix

--- src/photo/services/photo.service.ts
+++ src/photo/services/photo.service.ts
@@ -11,14 +11,14 @@ class PhotoService {
     // * PUBLIC: Fetch all photos for map visualization
     async getAll() {
         // * API Spec: GET /photos -> IPhotoRdo[]
-        const { data } = await axiosClassic.get<IPhotoRdo[]>(API_URL.files('/photos'))
+        const { data } = await axiosClassic.get<IPhotoRdo[]>(API_URL.root('/photos'))
         return data
     }
 
     // * PUBLIC
     async getById(photoId: string) {
         // * API Spec: GET /photos/:id -> IPhotoDetails
-        const { data } = await axiosClassic.get<IPhotoDetails>(API_URL.files(`/photos/${photoId}`))
+        const { data } = await axiosClassic.get<IPhotoDetails>(API_URL.root(`/photos/${photoId}`))
         return data
     }
 
@@ -39,7 +39,7 @@ class PhotoService {
         formData.append('longitude', metadata.longitude.toString())
 
         // * API Spec: POST /photos -> IPhoto (we assume IPhoto is just the created object, similar to IPhotoRdo without commentsCount)
-        const { data: createdPhoto } = await axiosWithAuth.post<IPhotoRdo>(API_URL.files('/photos'), formData, {
+        const { data: createdPhoto } = await axiosWithAuth.post<IPhotoRdo>(API_URL.root('/photos'), formData, {
             headers: {
                 // * Important: 'Content-Type' header is set automatically to 'multipart/form-data' 
                 // * For simplicity here, we let the browser handle it.
@@ -51,7 +51,7 @@ class PhotoService {
     // * PROTECTED
     async addComment(photoId: string, data: ICreateCommentForm) {
         // * API Spec: POST /photos/:photoId/comments -> ICommentRdo
-        const url = API_URL.files(`/photos/${photoId}/comments`)
+        const url = API_URL.root(`/photos/${photoId}/comments`)
         const response = await axiosWithAuth.post<ICommentRdo>(url, data)
         return response.data
     }
@@ -59,7 +59,7 @@ class PhotoService {
     // * PUBLIC
     async getCommentsByPhotoId(photoId: string) {
         // * API Spec: GET /photos/:photoId/comments -> ICommentRdo[]
-        const url = API_URL.files(`/photos/${photoId}/comments`)
+        const url = API_URL.root(`/photos/${photoId}/comments`)
         const { data } = await axiosClassic.get<ICommentRdo[]>(url)
         return data
     }
